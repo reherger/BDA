36b6d18f887348cc5a9709aedc61860db4e03ab6
reherger <reto.herger@iroval.ch>
2015-03-05 19:50:50 +0100

    xml reading concept

--- build/generated/source/r/debug/com/estimote/examples/demos/R.java
+++ build/generated/source/r/debug/com/estimote/examples/demos/R.java
@@ -39,21 +39,24 @@ public final class R {
     }
     public static final class layout {
         public static final int actionbar_indeterminate_progress=0x7f030000;
-        public static final int all_demos=0x7f030001;
-        public static final int characteristics_demo=0x7f030002;
-        public static final int device_item=0x7f030003;
-        public static final int distance_view=0x7f030004;
-        public static final int ins=0x7f030005;
-        public static final int main=0x7f030006;
-        public static final int notify_demo=0x7f030007;
+        public static final int activity_git_push_test=0x7f030001;
+        public static final int all_demos=0x7f030002;
+        public static final int characteristics_demo=0x7f030003;
+        public static final int device_item=0x7f030004;
+        public static final int distance_view=0x7f030005;
+        public static final int ins=0x7f030006;
+        public static final int main=0x7f030007;
+        public static final int notify_demo=0x7f030008;
     }
     public static final class menu {
-        public static final int menu_ins=0x7f060000;
-        public static final int scan_menu=0x7f060001;
+        public static final int menu_git_push_test=0x7f060000;
+        public static final int menu_ins=0x7f060001;
+        public static final int scan_menu=0x7f060002;
     }
     public static final class string {
         public static final int action_settings=0x7f050000;
         public static final int hello_world=0x7f050001;
-        public static final int title_activity_ins=0x7f050002;
+        public static final int title_activity_git_push_test=0x7f050002;
+        public static final int title_activity_ins=0x7f050003;
     }
 }
